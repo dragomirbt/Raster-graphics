4c89dc572a788f786dfb5973432a0f07d27d5ed6
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 18:30:26 +0300

    added documentation

--- header-files/Negative.h
+++ header-files/Negative.h
@@ -1,9 +1,13 @@
 #ifndef  NEGATIVE_HEADER_INCLUDED
 #define  NEGATIVE_HEADER_INCLUDED
 #include "Files.h"
+
+//класът съдържа командата "Negative"
+
 class Negative : public File
 {
 public:
+        // прави негатив на всички картинки в текущата сесия
 	void execute(Files& images);
 };
 #endif 
