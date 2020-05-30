dffa431fca846c45bc8a8512b52bacc27fb4b4db
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 18:06:13 +0300

    added documentation

--- header-files/Collage.h
+++ header-files/Collage.h
@@ -1,9 +1,13 @@
 #ifndef COLLAGE_HEADER_INCLUDED
 #define COLLAGE_HEADER_INCLUDED
 #include "Files.h"
+
+//класът съдържа командата "Collage"
+
 class Collage : public File
 {
 public:
+	// създава хоризонтален или вертикален колаж от две картинки и го доабавя в масива от картинки
 	void execute(const char* direction, const char* image1, const char* image2, Files& image);
 };
 #endif
