d1750c8dc1b5eb6abb1b9e641bfa18f11e22dc9d
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 18:35:41 +0300

    added documentation

--- header-files/Save.h
+++ header-files/Save.h
@@ -2,9 +2,12 @@
 #define SAVE_HEADER_INCLUDED
 #include "Files.h"
 
+// класът съдържа командата "Save"
+
 class Save : public File
 {
 public:
+	// запазва направените промени върху картинките в масива във файловете
 	bool execute(Files& images);
 };
 #endif
