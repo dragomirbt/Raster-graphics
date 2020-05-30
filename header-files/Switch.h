aa713e818a28023673da09db11793df5cf185506
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 18:43:21 +0300

    added documentation

--- header-files/Switch.h
+++ header-files/Switch.h
@@ -2,9 +2,13 @@
 #define SWITCH_HEADER_INCLUDED
 #include "SessionInfo.h"
 #include "Files.h"
+
+//класът съдържа командата "Switch"
+
 class Switch : public SessionInfo
 {
 public:
+	// превключва към друга сесия, според подаден индентификационен номер
 	void execute(Files& images, Transformations& transformations, int ID);
 };
 #endif
