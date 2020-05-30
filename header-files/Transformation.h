976534e365f2e14aed64c817503ffc1a9009ba51
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 18:46:20 +0300

    added documentation

--- header-files/Transformation.h
+++ header-files/Transformation.h
@@ -2,16 +2,27 @@
 #define TRANSFORMATION_HEADER_INCLUDED
 
 const int BUFF_SIZE = 64;
+
+//класът описва информацията, която съдържа една трансформация
+/* член данни:
+	- индентификационен номер
+	- име на трансформацията
+*/
 class Transformation
 {
 private:
 	int ID;
 	char transformation_name[BUFF_SIZE];
 public:
+	// конструктор по подаден индентификационен номер и име на трансформацията
 	Transformation(int ID, const char* transformation_name);
+	// променя индентификационния номер 
 	void setID(int ID);
+	// порменя името на трансформацията
 	void setTransformationName(const char* transformation_name);
+	// връща int индентификационния номер 
 	int getID() const;
+	// връща const char* името на трансформацията
 	const char* getTransformationName() const;
 };
 #endif
