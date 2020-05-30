e8fe6c0a195747629d14312e89cf17552f3a213e
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 17:57:37 +0300

    added documentation

--- header-files/BasicCommands.h
+++ header-files/BasicCommands.h
@@ -3,19 +3,33 @@
 #include<fstream>
 
 using namespace std;
+
+//класът се използва за работа с по-простите команди от командния ред (help,exit)
+//както и подобните команди (add,load)
+
+//член данни - име на командата
+
 const int MAX_COMMAND_LENGHT = 15;
 class BasicCommands
 {
 private:
 	char name[MAX_COMMAND_LENGHT];
 public:
+	//конструктор по подразбиране
 	BasicCommands();
+	//конструктор с параметър име на команда
 	BasicCommands(const char* name);
 	//basic comamnds
+	
+	//зарежда файл по нужния начин (binary, ascii)
 	bool load(const char* image_name);
+	//принтира командите, с които разполага приложението
 	void help() const;
+	// излиза от програмата
 	void exit() const;
 	//other commands
+	
+	// добавя/зарежда файл по нужния начин в текуща сесия
 	void add(const char* image_name);
 };
 #endif
