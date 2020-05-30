ce9d3f234ee35454f0e262095f3d59eb43828455
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 18:31:56 +0300

    added documentation

--- header-files/Rotate.h
+++ header-files/Rotate.h
@@ -2,9 +2,12 @@
 #define ROTATE_HEADER_INCLUDED
 #include "Files.h"
 
+//класът съдържа командата "Rotate"
+
 class Rotate : public File
 {
 public:
+   // завърта картинка на 90 градуса наляво или надясно
     void execute(Files& images, const char* direction);
 };
 #endif
