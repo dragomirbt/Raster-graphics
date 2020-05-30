455e7c22a90dd645581911b9ac20af03af71473e
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 18:21:19 +0300

    added documentation

--- header-files/Files.h
+++ header-files/Files.h
@@ -2,17 +2,23 @@
 #define FILES_HEADER_INCLUDED
 #include <vector>
 #include "File.h"
-
+ 
+// класът описва масив от картинки (File)
+//член данни: Масив от File
 class Files
 {
 private:
 	std::vector<File> array;
 public:
+	// предифиниран оператор []
 	File& operator[](int index);
+	// функция, която добавя картинка в масива
 	void addFile(File file);
+	//функция, която връща File последната добавена картинка в масива
 	File getLastAddedFile() const;
+	// функция, която връща броят на картинките в масива
 	int getSize() const;
+	// функция, която изтрива последната картинка от масива
 	void deleteLastFile();
 };
-
 #endif
