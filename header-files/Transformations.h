5fdf16b447bdd4eebc4c306408c095f477f50871
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 18:48:19 +0300

    added documentation

--- header-files/Transformations.h
+++ header-files/Transformations.h
@@ -3,14 +3,21 @@
 #include "Transformation.h"
 #include <vector>
 using namespace std;
+
+// класът описва масив от трансформации
+// член данни: масив от Transformation
 class Transformations
 {
 private:
 	vector<Transformation> transformations;
 public:
+	//добавя трансформация към масива
 	void addTransformation(Transformation trans);
+	// предифиниран оператор []
 	Transformation& operator[](int index);
+	// връша int текущият брой на трансформациите в масива
 	int getSize() const;
+	// изтрива трансформация в масива по подаден index
 	void deleteTransformation(int index);
 };
 #endif
