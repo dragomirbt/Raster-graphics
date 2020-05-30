07c0b2ca3c24017d796d1d5c8966d175a1e1380f
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 18:42:12 +0300

    added documentation

--- header-files/SessionInfo.h
+++ header-files/SessionInfo.h
@@ -2,9 +2,13 @@
 #define SESSION_INFO_HEADER_INCLUDED
 #include "Files.h"
 
+//класът съдържа командата "SessionInfo"
+
 class SessionInfo
 {
 public:
+    //извежда информация за текущата сесия (имената на изображения са в сесията, индентификационният номер на сесията,
+    //трансформациите, които предстоят да се извърщат
     void execute(Files& images, Transformations& transformations, int cur_ID);
 };
 #endif
