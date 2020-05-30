ecdd5289e99713b11749a3c5a5256cc2414e6035
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 18:17:38 +0300

    added documentation

--- header-files/File.h
+++ header-files/File.h
@@ -8,6 +8,16 @@ using namespace std;
 const int MAX_SIZE_FILE_FORMAT = 4;
 const int MAX_IMAGE_NAME_SIZE = 15;
 
+// класът описва информацията която съдържа една картинка
+/* член данни:
+	- формат на картинката
+	- ширина
+	- височина
+	- максимална стойност за цвят
+	- матрица
+	- индентификационен номер
+	- име на картинката
+*/
 class File
 {
 private:
@@ -19,27 +29,47 @@ private:
 	int ID;
 	char image_name[MAX_IMAGE_NAME_SIZE];
 protected:
+	// взема разширението на дадена картинка по подадено име
 	virtual char* getExtention(const char* image_name) const;
 public:
+	// конструктор по подразбиране
 	File();
+	// конструктор по подадено име на картинка
 	File(const char* image_name);
+	// копи конструктор
 	File(const File& other);
+	// предефиниран оператор за присвояване
 	File& operator=(const File& other);
+	// деструктор
 	~File();
 	
+	// променя формата на картинката
 	void setFileFormat(const char* file_format);
+	// променя широчината на картинката
 	void setWidth(unsigned width);
+	// променя височината на картинката
 	void setHeight(unsigned height);
+	// променя максималната стойност за цвят на картинката
 	void setMaxValueForColor(unsigned max_value_for_color);
+	// променя сойностите в матрицата на картинката
 	void setMatrix(int** matrix);
+	// променя индентификационния номер на картинката
 	void setID(int ID);
+	// връша const char* формат на картинката
 	const char* getFileFormat() const;
+	// връща unsigned широчина на картинката
 	unsigned getWidth() const;
+	//връща unsigned височина на картинката
 	unsigned getHeight() const;
+	//връща unsigned максимална стойност за цвят на картинката
 	unsigned getMaxValueForColor() const;
+	// връща int** матрица на картинката
 	int** getMatrix() const;
+	// връща int индентификационен номер на картинката
 	int getID() const;
+	// връша const char* име на картинката
 	const char* getImageName() const;
+	// променя името на картинката
 	void setImageName(const char* image_name);	
 };
 
