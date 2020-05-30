93b60ecab9efe61591349bf229468485f4b4236b
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 18:29:15 +0300

    added documentation

--- header-files/Monochrome.h
+++ header-files/Monochrome.h
@@ -2,9 +2,12 @@
 #define MONOCHROME_HEADER_INCLUDED
 #include "Files.h"
 
+// класът съдържа командата "Monochrome"
+
 class Monochrome : public File
 {
 public:
+    // всички картинки в текущата сесия стават черно-бели
     void execute(Files& images);
 };
 #endif
