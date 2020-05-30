aa971e1f3f0588594adadb0f0a23119272c7fb49
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-30 18:25:30 +0300

    added documentation

--- header-files/Grayscale.h
+++ header-files/Grayscale.h
@@ -1,9 +1,13 @@
 #ifndef GRAYSCALE_HEADER_INCLUDED
 #define GRAYSCALE_HEADER_INCLUDED
 #include "Files.h"
+
+//класът съдържа командата "Grayscale"
+
 class Grayscale : public File
 {
 public:
+       // прави всички картинки в текущата сесия с нюанси на сивото
 	void execute(Files& images);
 };
 #endif
