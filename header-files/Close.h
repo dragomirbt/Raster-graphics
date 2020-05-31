577386b5fde113382d35185b7af9911e79396386
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-31 13:59:24 +0300

    very last update

--- header-files/Close.h
+++ header-files/Close.h
@@ -2,14 +2,12 @@
 #define CLOSE_HEADER_INCLUDED
 #include "Files.h"
 
-//класът съдържа командата "Close"
-
+/// класът съдържа командата "Close"
 class Close
 {
 public:
-	//изтрива последно добавеният файл в масива от файлове
-	//@param images Масив от картинки
+	/// изтрива последно добавеният файл в масива от файлове
+	/// @param images Масив от картинки
 	void execute(Files& images);
 };
-
 #endif
