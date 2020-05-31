d01640b439aa99f1b3719d86880190ea06dada2d
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-31 14:04:59 +0300

    very last update

--- header-files/Undo.h
+++ header-files/Undo.h
@@ -6,14 +6,13 @@
 #include "Negative.h"
 #include "Rotate.h"
 
-//класът съдържа командата "Undo"
-
+/// класът съдържа командата "Undo"
 class Undo
 {
 public:
-	// връща всички картинки в текущата сесия в масива към предишният им вид
-	// @param transformations масив от трансформации
-	// @param images масив от картинки
+	/// връща всички картинки в текущата сесия в масива към предишният им вид
+	/// @param transformations масив от трансформации
+	/// @param images масив от картинки
 	void execute(Transformations& transformations, Files& images);
 };
 #endif
