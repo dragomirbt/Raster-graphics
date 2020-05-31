7a5ba3746380045c9f2497538cc34b7b14a95a30
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-31 14:01:31 +0300

    very last update

--- header-files/Monochrome.h
+++ header-files/Monochrome.h
@@ -2,13 +2,12 @@
 #define MONOCHROME_HEADER_INCLUDED
 #include "Files.h"
 
-// класът съдържа командата "Monochrome"
-
+/// класът съдържа командата "Monochrome"
 class Monochrome : public File
 {
 public:
-    // всички картинки в текущата сесия стават черно-бели
-    // @param images масив от картинки
+    /// всички картинки в текущата сесия стават черно-бели
+    /// @param images масив от картинки
     void execute(Files& images);
 };
 #endif
