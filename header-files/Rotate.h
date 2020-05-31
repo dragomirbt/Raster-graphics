9fcd81639ee567f68651c8f4406a6463ae3a1762
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-31 11:32:40 +0300

    last update

--- header-files/Rotate.h
+++ header-files/Rotate.h
@@ -8,6 +8,8 @@ class Rotate : public File
 {
 public:
    // завърта всички картинки на 90 градуса в текущата сесия наляво или надясно
+    // @param images масив от картинки
+    // @param direction посока в която се завърта картинката    
     void execute(Files& images, const char* direction);
 };
 #endif
