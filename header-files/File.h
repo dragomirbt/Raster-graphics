839f1b0bc452371ca7c82d4374ff85c06a57458c
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-31 13:24:54 +0300

    very last update

--- header-files/File.h
+++ header-files/File.h
@@ -8,16 +8,17 @@ using namespace std;
 const int MAX_SIZE_FILE_FORMAT = 4;
 const int MAX_IMAGE_NAME_SIZE = 15;
 
-// класът описва информацията която съдържа една картинка
-/* член данни:
-	- формат на картинката
-	- ширина
-	- височина
-	- максимална стойност за цвят
-	- матрица
-	- индентификационен номер
-	- име на картинката
-*/
+/// класът описва информацията която съдържа една картинка
+///
+/// член данни:
+///	- формат на картинката
+///	- ширина
+///	- височина
+///	- максимална стойност за цвят
+///	- матрица
+///	- индентификационен номер
+///	- име на картинката
+
 class File
 {
 private:
@@ -29,55 +30,55 @@ private:
 	int ID;
 	char image_name[MAX_IMAGE_NAME_SIZE];
 protected:
-	// взема разширението на дадена картинка по подадено име
-	// @param  image_name Името на картинката
+	/// взема разширението на дадена картинка по подадено име
+	/// @param  image_name Името на картинката
 	virtual char* getExtention(const char* image_name) const;
 public:
-	// конструктор по подразбиране
+	/// конструктор по подразбиране
 	File();
-	// конструктор по подадено име на картинка
+	/// конструктор по подадено име на картинка
 	File(const char* image_name);
-	// копи конструктор
+	/// копи конструктор
 	File(const File& other);
-	// предефиниран оператор за присвояване
+	/// предефиниран оператор за присвояване
 	File& operator=(const File& other);
-	// деструктор
+	/// деструктор
 	~File();
 
-	// променя формата на картинката
-	// @param  file_format форматът на картинката
+	/// променя формата на картинката
+	/// @param  file_format форматът на картинката
 	void setFileFormat(const char* file_format);
-	// променя широчината на картинката
-	// @param  width широчината на картинката
+	/// променя широчината на картинката
+	/// @param  width широчината на картинката
 	void setWidth(unsigned width);
-	// променя височината на картинката
-	// @param  height височината на картинката
+	/// променя височината на картинката
+	/// @param  height височината на картинката
 	void setHeight(unsigned height);
-	// променя максималната стойност за цвят на картинката
-	// @param  max_value_for_color максималната стойност за цвят на картинката
+	/// променя максималната стойност за цвят на картинката
+	/// @param  max_value_for_color максималната стойност за цвят на картинката
 	void setMaxValueForColor(unsigned max_value_for_color);
-	// променя сойностите в матрицата на картинката
-	// @param  matrix матрицата на картинката
+	/// променя сойностите в матрицата на картинката
+	/// @param  matrix матрицата на картинката
 	void setMatrix(int** matrix);
-	// променя индентификационния номер на картинката
-	// @param  ID индентификационният на картинката
+	/// променя индентификационния номер на картинката
+	/// @param  ID индентификационният на картинката
 	void setID(int ID);
-	// връша const char* формат на картинката
+	/// @returns const char* формат на картинката
 	const char* getFileFormat() const;
-	// @returns unsigned широчина на картинката
+	/// @returns unsigned широчина на картинката
 	unsigned getWidth() const;
-	//@returns unsigned височина на картинката
+	/// @returns unsigned височина на картинката
 	unsigned getHeight() const;
-	//@returns unsigned максимална стойност за цвят на картинката
+	/// @returns unsigned максимална стойност за цвят на картинката
 	unsigned getMaxValueForColor() const;
-	// @returns int** матрица на картинката
+	/// @returns int** матрица на картинката
 	int** getMatrix() const;
-	// @returns int индентификационен номер на картинката
+	/// @returns int индентификационен номер на картинката
 	int getID() const;
-	// @returns const char* име на картинката
+	/// @returns const char* име на картинката
 	const char* getImageName() const;
-	// променя името на картинката
-	// @param image_name име на картинката
+	/// променя името на картинката
+	/// @param image_name име на картинката
 	void setImageName(const char* image_name);
 };
 #endif
