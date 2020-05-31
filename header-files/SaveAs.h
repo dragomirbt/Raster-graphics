141528624e2e7e982b5e1b4473f6039e6952fa13
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-31 11:34:52 +0300

    last update

--- header-files/SaveAs.h
+++ header-files/SaveAs.h
@@ -8,6 +8,8 @@ class SaveAs : public File
 {
 public:
 	// запазва досега напрвените промени върху картинките в текущата сесия във файлове в нова директория
+	// @param images масив от картинки
+	// @param image_path пътят където трябва да се запази картинката 
 	bool execute(Files& images, const char* image_path);
 };
 #endif
