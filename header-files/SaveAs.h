563cb57af77c17f21d51d1a19ea1023d8cfaae09
dragosa4a <57138353+dragosa4a@users.noreply.github.com>
2020-05-31 14:02:59 +0300

    very last update

--- header-files/SaveAs.h
+++ header-files/SaveAs.h
@@ -2,14 +2,13 @@
 #define SAVEAS_HEADER_INCLUDED
 #include "Files.h"
 
-//класът съдържа командата "Saveas"
-
+/// класът съдържа командата "Saveas"
 class SaveAs : public File
 {
 public:
-	// запазва досега напрвените промени върху картинките в текущата сесия във файлове в нова директория
-	// @param images масив от картинки
-	// @param image_path пътят където трябва да се запази картинката 
+	/// запазва досега напрвените промени върху картинките в текущата сесия във файлове в нова директория
+	/// @param images масив от картинки
+	/// @param image_path пътят където трябва да се запази картинката 
 	bool execute(Files& images, const char* image_path);
 };
 #endif
